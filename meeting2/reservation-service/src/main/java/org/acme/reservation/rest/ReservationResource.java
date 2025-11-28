e8f15c5bff5624b06c8b72bc7b1f0f92bc19bb27
Fredrik Duprez <fredrik@duprez.org>
2025-11-28 15:41:50 +0100

    kafta added

--- meeting2/reservation-service/src/main/java/org/acme/reservation/rest/ReservationResource.java
+++ meeting2/reservation-service/src/main/java/org/acme/reservation/rest/ReservationResource.java
@@ -4,33 +4,42 @@ import io.quarkus.hibernate.reactive.panache.common.WithTransaction;
 import io.quarkus.logging.Log;
 import io.smallrye.graphql.client.GraphQLClient;
 import io.smallrye.mutiny.Uni;
+import io.smallrye.reactive.messaging.MutinyEmitter;
 import jakarta.inject.Inject;
-import jakarta.transaction.Transactional;
 import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import org.acme.reservation.billing.Invoice;
+import org.acme.reservation.entity.Reservation;
 import org.acme.reservation.inventory.Car;
 import org.acme.reservation.inventory.GraphQLInventoryClient;
 import org.acme.reservation.inventory.InventoryClient;
-import org.acme.reservation.rental.Rental;
 import org.acme.reservation.rental.RentalClient;
-import org.acme.reservation.entity.Reservation;
+import org.eclipse.microprofile.reactive.messaging.Channel;
 import org.eclipse.microprofile.rest.client.inject.RestClient;
 import org.jboss.resteasy.reactive.RestQuery;
 
 import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+
 @Path("reservation")
 @Produces(MediaType.APPLICATION_JSON)
 public class ReservationResource {
+
+    public static final double STANDARD_RATE_PER_DAY = 19.99;
+    @Inject
+    @Channel("invoices")
+    MutinyEmitter<Invoice> invoiceEmitter;
+
     private final InventoryClient inventoryClient;
     private final RentalClient rentalClient;
     @Inject
@@ -49,18 +58,28 @@ public class ReservationResource {
     public Uni<Reservation> make(Reservation reservation) {
         reservation.userId = context.getUserPrincipal() != null ?
                 context.getUserPrincipal().getName() : "anonymous";
+
         return reservation.<Reservation>persist().onItem()
                 .call(persistedReservation -> {
                     Log.info("Successfully reserved reservation "
                             + persistedReservation);
+
+                    Uni<Void> invoiceUni = invoiceEmitter.send(
+                                    new Invoice(reservation, computePrice(reservation)))
+                            .onFailure().invoke(throwable ->
+                                    Log.errorf("Couldn't create invoice for %s. %s%n",
+                                            persistedReservation, throwable.getMessage()));
+
                     if (persistedReservation.startDay.equals(LocalDate.now())) {
-                        return rentalClient.start(persistedReservation.userId,
-                                        persistedReservation.id)
-                                .onItem().invoke(rental ->
-                                        Log.info("Successfully started rental " + rental))
-                                .replaceWith(persistedReservation);
+                        return invoiceUni.chain(() ->
+                                rentalClient.start(persistedReservation.userId,
+                                                persistedReservation.id)
+                                        .onItem().invoke(rental ->
+                                                Log.info("Successfully started rental " + rental))
+                                        .replaceWith(persistedReservation));
                     }
-                    return Uni.createFrom().item(persistedReservation);
+                    return invoiceUni
+                            .replaceWith(persistedReservation);
                 });
     }
 
@@ -88,6 +107,7 @@ public class ReservationResource {
                     return carsById.values();
                 });
     }
+
     @GET
     @Path("all")
     public Uni<Collection<Reservation>> allReservations() {
@@ -98,5 +118,10 @@ public class ReservationResource {
                         .filter(reservation -> userId == null ||
                                 userId.equals(reservation.userId))
                         .collect(Collectors.toList()));
+    }
+
+    private double computePrice(Reservation reservation) {
+        return (ChronoUnit.DAYS.between(reservation.startDay,
+                reservation.endDay) + 1) * STANDARD_RATE_PER_DAY;
     }
 }
